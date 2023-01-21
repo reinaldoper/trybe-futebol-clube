441dfdcd8c222673692f70f1ae5bfe2584cb8309
reinaldoper <reinaldoper83@gmail.com>
2023-01-21 15:59:44 -0400

    fix: continuidade na implementação dos testes do login

--- app/backend/src/tests/tests.login.test.ts
+++ app/backend/src/tests/tests.login.test.ts
@@ -25,6 +25,40 @@ describe('Definir o teste da rota de login', () => {
       expect(chaiHttpResponse.status).to.be.equal(200);
       expect(user.username).to.be.equal('Admin');    
     });
+    it('Nenhum email informado no corpo da requisição', async () => {
+      chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: '', password: 'secret_admin' });
+
+      const { message } = chaiHttpResponse.body;
+
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(message).to.be.equal('All fields must be filled');    
+    });
+    it('Nenhum password é informado no corpo da requisição', async () => {
+      chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: '' });
+
+      const { message } = chaiHttpResponse.body;
+
+      expect(chaiHttpResponse.status).to.be.equal(400);
+      expect(message).to.be.equal('All fields must be filled');    
+    });
+    it('Senha passada incorretamente no corpo da requisição', async () => {
+      chaiHttpResponse = await chai
+      .request(app)
+      .post('/login')
+      .send({ email: 'admin@admin.com', password: 's123er4' });
+
+      const { message } = chaiHttpResponse.body;
+
+
+      expect(chaiHttpResponse.status).to.be.equal(401);
+      expect(message).to.be.equal('Incorrect email or password');    
+    });
 
   });
 });
